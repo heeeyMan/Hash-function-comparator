6edf06b0db47282d50940b06dfad65337960eb52
Alexander <sania-vs.vasin@yandex.ru>
2024-04-13 02:42:35 +0300

    обновил gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,18 +42,18 @@ android {
     }
     buildFeatures.prefab = true
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
         aidl = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
+        kotlinCompilerExtensionVersion = "1.5.3"
     }
     packaging {
         resources {
